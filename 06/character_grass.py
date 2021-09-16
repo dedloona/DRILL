0cbd564dac9d2f002ffb26b4bc06c2b1e6bbb7cc
Yang <deadloona@kpu.ac.kr>
2021-09-16 19:15:05 +0900

    #06_Drill_circle_move
    원 운동 까지 성공

--- 06/character_grass.py
+++ 06/character_grass.py
@@ -5,8 +5,7 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-grass.draw_now(400, 30)
-character.draw_now(400, 90)
+
 
 x=0
 
--- 06/character_moves_circle.py
+++ 06/character_moves_circle.py
@@ -9,18 +9,18 @@ character = load_image('character.png')
 x = 400
 y = 90
 
-def ch_move(x0, y=90):
+def ch_move(x, y):
     clear_canvas_now()
     grass.draw_now(400, 30)
     character.draw_now(x, y)
-    delay(0.001)
+    delay(0.01)
+while True:
+    for i in range(100):
+        angle =  i * 2 * math.pi / 100
+        x = (math.cos(angle) * 350)+400
+        y = (math.sin(angle) * 250)+300
+        ch_move(x,y)
+    i = 0
 
-grass.draw_now(400, 30)
-character.draw_now(x, y)                       
-                                
 
-delay(3)          
-    
-    
-    
 close_canvas()
