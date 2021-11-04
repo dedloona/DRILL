5089b7068e81f62e5c9e4ea1d99157b76205d01a
Yang <deadloona@kpu.ac.kr>
2021-11-04 17:58:55 +0900

    들여쓰기 고침

--- Lecture11_Character_Controller/boy.py
+++ Lecture11_Character_Controller/boy.py
@@ -31,18 +31,19 @@ class IdleState:
         elif event == LEFT_UP:
             boy.velocity += 1
             boy.timer = 1000
-        def exit(boy, event):
+    def exit(boy, event):
             pass
-        def do(boy):
-            boy.frame = (boy.frame + 1) % 8
-            boy.timer -= 1
-            if boy.timer == 0:
-                boy.add_event(SLEEP_TIMER)
-        def draw(boy):
-            if boy.dir == 1:
-                boy.image.clip_draw(boy.frame * 100, 300, 100, 100, boy.x, boy.y)
-            else:
-                boy.image.clip_draw(boy.frame * 100, 200, 100, 100, boy.x, boy.y)
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.timer -= 1
+        if boy.timer == 0:
+            boy.add_event(SLEEP_TIMER)
+    def draw(boy):
+        if boy.dir == 1:
+            boy.image.clip_draw(boy.frame * 100, 300, 100, 100, boy.x, boy.y)
+        else:
+            boy.image.clip_draw(boy.frame * 100, 200, 100, 100, boy.x, boy.y)
+
 class RunState:
     def enter(boy, event):
         if event == RIGHT_DOWN:
@@ -54,18 +55,18 @@ class RunState:
         elif event == LEFT_UP:
             boy.velocity += 1
             boy.dir = boy.velocity
-        def exit(boy, event):
-            pass
-        def do(boy):
-            boy.frame = (boy.frame + 1) % 8
-            boy.timer -= 1
-            boy.x += boy.velocity
-            boy.x = clamp(25, boy.x, 800 - 25)
-        def draw(boy):
-            if boy.velocity == 1:
-                boy.image.clip_draw(boy.frame * 100, 100, 100, 100, boy.x, boy.y)
-            else:
-                boy.image.clip_draw(boy.frame * 100, 0, 100, 100, boy.x, boy.y)
+    def exit(boy, event):
+        pass
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.timer -= 1
+        boy.x += boy.velocity
+        boy.x = clamp(25, boy.x, 800 - 25)
+    def draw(boy):
+        if boy.velocity == 1:
+            boy.image.clip_draw(boy.frame * 100, 100, 100, 100, boy.x, boy.y)
+        else:
+            boy.image.clip_draw(boy.frame * 100, 0, 100, 100, boy.x, boy.y)
 
 class SleepState:
     def enter(boy, event):
@@ -111,7 +112,6 @@ class Boy:
         self.event_que = []
         self.cur_state = IdleState
         self.cur_state.enter(self, None)
-        pass
 
 
     def change_state(self,  state):
@@ -131,17 +131,12 @@ class Boy:
             self.cur_state = next_state_table[self.cur_state][event]
             self.cur_state.enter(self, event)
 
-
-
     def draw(self):
         self.cur_state.draw(self)
-
-
 
     def handle_event(self, event):
         if (event.type, event.key) in key_event_table:
             key_event = key_event_table[(event.type, event.key)]
             self.add_event(key_event)
 
-        pass
 
